bbf4d3234be27574a48e696567d3b99c366eee9e
shumez <shumelancholie+dev@gmail.com>
2023-02-20 21:32:53 +0900

    v1.0.1

--- code/pipeline_demo.sh
+++ code/pipeline_demo.sh
@@ -3,40 +3,31 @@
 # Project: 	/Volumes/BA46/ws/msthk/code
 # Authors: 	shumez <https://github.com/shumez>
 # Created: 	2023-02-16 21:16:00
-# Modified:	2023-02-19 15:33:12
-# Hosted:  	shuBookPro.local
+# Modified:	2023-02-20 21:31:34
 # -----
 # Copyright (c) 2023 shumez
 
 # preferences: to be modified
 DISK=BA46
 PROJ=msthk
-INST=2
-ID=9999
-N=1
+INST=00
+ID=0
+NRUN=1
 
-
+SSDIR=/Volumes/BA46/ws/msthk
+# ROOTDIR=/Volumes/$DISK
+# WSDIR=$ROOTDIR/ws
+# SSDIR=$WSDIR/$PROJ
 
 # preferences: directry
-ROOTDIR=/Volumes/$DISK
-WSDIR=$ROOTDIR/ws
-
-SSDIR=$WSDIR/$PROJ
-SRCDIR=$WSDIR/$PROJ/sourcedata
+SRCDIR=$SSDIR/sourcedata
 DCMDIR=$SRCDIR/dcm
-DERDIR=$WSDIR/$PROJ/derivatives
+DERDIR=$SSDIR/derivatives
 
 D2NVER=v1.0.20210317
 D2NDIR=$SRCDIR/dcm2niix/$D2NVER
 NIIDIR=$SRCDIR/nii
 LIST=$SSDIR/participants.tsv
-
-# MS pipeline
-MS_PIPELINE_VER=1.0.0
-MPDIR=$DERDIR/ms_pipeline/$MS_THK_PIPELINE_VER
-mkdir -p $MPDIR/$SID
-
-
 
 # padding
 INST="00${INST}"
@@ -44,12 +35,13 @@ INST="${INST: -2}"
 ID="00000000${ID}"
 ID="${ID: -8}"
 SID=sub-${INST}${ID}
-N="00${N}"
-NRUN="${N: -2}"
+NRUN="00${NRUN}"
+NRUN="${NRUN: -2}"
 
-
-
-
+# MS pipeline
+MS_PIPELINE_VER=1.0.1
+MPDIR=$DERDIR/ms_pipeline/$MS_THK_PIPELINE_VER
+mkdir -p $MPDIR/$SID/$NRUN
 
 # ws/msthk
 # ├── CHANGES
@@ -77,15 +69,15 @@ FS_VER=7.3.2
 # cd ~/freesurfer
 export FREESURFER_HOME=$HOME/freesurfer/${FS_VER}/freesurfer
 # export FREESURFER_HOME=/Volumes/BA46/bin/freesurfer/7.2.0/freesurfer
-export SUBJECTS_DIR=${WSDIR}/${PROJ}/derivatives/freesurfer/${FS_VER}
+export SUBJECTS_DIR=${SSDIR}/derivatives/freesurfer/${FS_VER}
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
 which freeview
 
 # print log: setup
 timestamp=`date +"%Y-%m-%d %T"`
-echo -e "${timestamp}\tfreesurfer\tsetup done\n" > $MPDIR/$SID/msthk_pipeline_log.txt
-echo -e "${SID}\t${timestamp}\t${MS_THK_PIPELINE_VER}\t" >> $SSDIR/msthk_pipeline_log.txt
+echo -e "${timestamp}\tfreesurfer\tsetup done" > $MPDIR/$SID/msthk_pipeline_log.txt
+echo -e -n "${SID}\t${timestamp}\t${MS_THK_PIPELINE_VER}\t" >> $SSDIR/msthk_pipeline_log.txt
 
 # copy disk
 mkdir -p $DCMDIR/$SID/$NRUN
@@ -93,8 +85,8 @@ cp -R /Volumes/AOC_MINI/* $DCMDIR/$SID/$NRUN
 
 # print log: copy disk
 timestamp=`date +"%Y-%m-%d %T"`
-echo -e "${timestamp}\tcopy disk\tdone\n" > $MPDIR/$SID/msthk_pipeline_log.txt
-echo -e "${timestamp}\t${D2NVER}\t${DCMDIR}/${SID}/${NRUN}\t" >> $SSDIR/msthk_pipeline_log.txt
+echo -e "${timestamp}\tcopy disk\tdone" >> $MPDIR/$SID/msthk_pipeline_log.txt
+echo -e -n  "${timestamp}\t\t${DCMDIR}/${SID}/${NRUN}\t" >> $SSDIR/msthk_pipeline_log.txt
 
 # dicom import
 mkdir -p ${D2NDIR}/${SID}/${NRUN}
@@ -102,21 +94,20 @@ dcm2niix -z y -f %p -o ${D2NDIR}/${SID}/${NRUN} ${DCMDIR}/${SID}/${NRUN}
 
 # print log: dicom import
 timestamp=`date +"%Y-%m-%d %T"`
-echo -e "${timestamp}\tdcm2niix\tdone\n" > $MPDIR/$SID/msthk_pipeline_log.txt
-echo -e "${timestamp}\t${D2NVER}\t${D2NDIR}/${SID}/${NRUN}\t" >> $SSDIR/msthk_pipeline_log.txt
+echo -e "${timestamp}\tdcm2niix\tdone" >> $MPDIR/$SID/msthk_pipeline_log.txt
+echo -e -n  "${timestamp}\t${D2NVER}\t${D2NDIR}/${SID}/${NRUN}\t" >> $SSDIR/msthk_pipeline_log.txt
 
 # copy to bids folder
-
 cp ${D2NDIR}/${SID}/${NRUN}/t1_mprage_sag_*.json ${SSDIR}/${SID}/anat/${SID}-${NRUN}_T1w.json
-cp ${D2NDIR}/${SID}/${NRUN}/t1_mprage_sag_*.nii.gz ${SSDIR}/${SID}/anat/${SID}-${NRUN}_T1w.gz
+cp ${D2NDIR}/${SID}/${NRUN}/t1_mprage_sag_*.nii.gz ${SSDIR}/${SID}/anat/${SID}-${NRUN}_T1w.nii.gz
 
 cp ${D2NDIR}/${SID}/${NRUN}/flair_space_sag_*.json ${SSDIR}/${SID}/anat/${SID}-${NRUN}_FLAIR.json
 cp ${D2NDIR}/${SID}/${NRUN}/flair_space_sag_*.nii.gz ${SSDIR}/${SID}/anat/${SID}-${NRUN}_FLAIR.nii.gz
 
 # print log: dicom import
 timestamp=`date +"%Y-%m-%d %T"`
-echo -e "${timestamp}\tcopy nii\tdone\n" > $MPDIR/$SID/msthk_pipeline_log.txt
-echo -e "${timestamp}\t\t${SSDIR}/${SID}/anat/\t" >> $SSDIR/msthk_pipeline_log.txt
+echo -e "${timestamp}\tcopy nii\tdone" >> $MPDIR/$SID/msthk_pipeline_log.txt
+echo -e -n  "${timestamp}\t\t${SSDIR}/${SID}/anat/\t" >> $SSDIR/msthk_pipeline_log.txt
 
 
 # freesurfer reconstruction all
@@ -129,30 +120,29 @@ recon-all \
 
 # print log
 timestamp=`date +"%Y-%m-%d %T"`
-echo -e "${timestamp}\tfreesurfer\trecon-all done\n" > $MPDIR/$SID/msthk_pipeline_log.txt
-echo -e "${timestamp}\t${FS_VER}\t${SUBJECTS_DIR}/${SID}\t" >> $SSDIR/msthk_pipeline_log.txt
-
+echo -e "${timestamp}\tfreesurfer\trecon-all done" >> $MPDIR/$SID/msthk_pipeline_log.txt
+echo -e -n  "${timestamp}\t${FS_VER}\t${SUBJECTS_DIR}/${SID}\t" >> $SSDIR/msthk_pipeline_log.txt
 
 
 # freesurfer segmentation extra
 ${FREESURFER_HOME}/bin/segmentBS.sh $SID $SUBJECTS_DIR
 # print log
 timestamp=`date +"%Y-%m-%d %T"`
-echo -e "${timestamp}\tfreesurfer\tsegmentBS done\n" > $MPDIR/$SID/msthk_pipeline_log.txt
-echo -e "${timestamp}\t${FS_VER}\t${SUBJECTS_DIR}/${SID}\t" >> $SSDIR/msthk_pipeline_log.txt
+echo -e "${timestamp}\tfreesurfer\tsegmentBS done" >> $MPDIR/$SID/msthk_pipeline_log.txt
+echo -e -n "${timestamp}\t${FS_VER}\t${SUBJECTS_DIR}/${SID}\t" >> $SSDIR/msthk_pipeline_log.txt
 
 ${FREESURFER_HOME}/bin/segmentHA_T1.sh $SID $SUBJECTS_DIR
 
 # print log
 timestamp=`date +"%Y-%m-%d %T"`
-echo -e "${timestamp}\tfreesurfer\tsegmentHA done\n" > $MPDIR/$SID/msthk_pipeline_log.txt
-echo -e "${timestamp}\t${FS_VER}\t" >> $SSDIR/msthk_pipeline_log.txt
+echo -e "${timestamp}\tfreesurfer\tsegmentHA done" >> $MPDIR/$SID/msthk_pipeline_log.txt
+echo -e -n  "${timestamp}\t${FS_VER}\t" >> $SSDIR/msthk_pipeline_log.txt
 
 ${FREESURFER_HOME}/bin/segmentThalamicNuclei.sh $SID $SUBJECTS_DIR
 
 # print log
 timestamp=`date +"%Y-%m-%d %T"`
-echo -e "${timestamp}\tfreesurfer\tsegmentThalamicNuclei done\n" > $MPDIR/$SID/msthk_pipeline_log.txt
+echo -e "${timestamp}\tfreesurfer\tsegmentThalamicNuclei done" >> $MPDIR/$SID/msthk_pipeline_log.txt
 echo -e "${timestamp}\t${FS_VER}\t" >> $SSDIR/msthk_pipeline_log.txt
 
 
